3aee8e8ff5ec071169d69f7b273a06b7ec07e53c
Khamari <khamariellison@gmail.com>
2025-09-18 09:52:44 -0400

    Commit Message

--- src/JavaNumerics.java
+++ src/JavaNumerics.java
@@ -1,9 +1,26 @@
+import java.sql.SQLOutput;
 
 public class JavaNumerics
-    public static void main(String[] args)
+{
+	public static void main(String[] args)
+	{
+        int intOperandA = 1;
+        int intOperandB = 2;
+        int intSum = 3;
+        int intProduct = 4;
+        int intDifference = 5;
+        int intQuotient = 6;
+        int intModulo = 7;
 
-        System.out.printf("Hello and welcome!");
+        double doubleOperandA = 1.0;
+        double doubleOperandB = 2.0;
+        double doubleSum = 3.0;
+        double doubleProduct = 4.0;
+        double doubleDifference = 5.0;
+        double doubleQuotient = 6.0;
 
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of" + intOperandA + " " + intOperandB + " is " + intSum);
 
-
+	}
 }
